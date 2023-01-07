b19858d8148bc66bb81e39feef9c335a86d2289b
Nikhil4474 <ninuganti15@gmail.com>
2023-01-06 18:14:33 -0600

    first commit

---
+++ errorhandler.py
@@ -0,0 +1,15 @@
+# Syntax Error, Error when wrong syntax
+# a = 8
+# b = 8
+# c = a b
+
+# Zero Division Error, Error when division by zero
+# number=[5,0,2]
+# for i in number:
+#   try:
+#     result = 5/i
+#     print("5 divided by",i,"is: ", result)
+#   except ZeroDivisionError:
+#     print("Cannot divide by zero")
+
+# Keyboard Interrupt Error, when user interrupts with keyboard (ctrl+c)
---
+++ f.py
@@ -0,0 +1,4 @@
+import os
+p = r"C:/Users/Nikhil/Downloads/Move_To/"
+
+print(os.path.exists(p))
